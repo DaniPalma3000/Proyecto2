c11a207a5111481d612c26a04d2c0e78839596e3
TheDani3000 <98671625+DaniPalma3000@users.noreply.github.com>
2025-05-25 23:30:03 -0600

    Agregar rutas para obtener todos los empleados y empleados por departamento en el módulo de empleados

--- backend/routes/empleado.js
+++ backend/routes/empleado.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registrarEmpleado, obtenerDescriptores, listarEmpleados } = require('../controllers/empleadoController');
+const { registrarEmpleado, obtenerDescriptores, listarEmpleados, obtenerTodosLosEmpleados, obtenerEmpleadosPorDepartamento } = require('../controllers/empleadoController');
 const router = express.Router();
 
 router.post('/empleado', registrarEmpleado);
 router.get('/empleado', listarEmpleados);
 router.get('/descriptores', obtenerDescriptores);
+router.get('/', obtenerTodosLosEmpleados); //all employees
+router.get('/por-departamento/:id', obtenerEmpleadosPorDepartamento);
 
 module.exports = router;
